8aeda44f268d1d069c1d02b61e1522d543e8217a
andreaortega <andreaortegadumont@gmail.com>
2017-10-08 20:33:38 +0200

    4) Funciona el cuadro de diálogo con el LongClick

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -1,5 +1,7 @@
 package edu.upc.eseiaat.pma.countrylist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -41,9 +43,23 @@ public class CountryListActivity extends AppCompatActivity {
 
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View item, int pos, long id) {
-                country_list.remove(pos);
-                adapter.notifyDataSetChanged();
+            public boolean onItemLongClick(AdapterView<?> parent, View item, final int pos, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(CountryListActivity.this);
+                builder.setTitle(R.string.confirmation);
+                String msg = getResources().getString(R.string.confirmation_message);
+                builder.setMessage(msg+ " " + country_list.get(pos)+ "?");
+
+                builder.setPositiveButton(R.string.erase, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        country_list.remove(pos);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton(android.R.string.cancel,null);
+                builder.create().show();
                 return true; //Ahi sale false, si le ponemos TRUE es para que el click normal no ocurra
             }
         });
